d9ac41a8838e5fcb399fecb9ded117b4dcf25aa2
Sadie <sadiefreeman@gmail.com>
2018-03-19 19:22:00 +0000

    Added a new string and added to the data

---
+++ printing-exercise/print-buffered-strings.js
@@ -0,0 +1,26 @@
+var https = require('https');
+
+
+function getAndPrintHTML () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
+
+ https.get(requestOptions, function (response) {
+    var newData = "";
+    response.setEncoding('utf8');
+
+    response.on('data', function (data) {
+      console.log(data += newData);
+    });
+
+    response.on('end', function () {
+      console.log('Response stream complete.')
+    });
+
+ })
+
+}
+getAndPrintHTML();
