781542de0db106314edf26d6f62f2f21dfb167e6
Sadie <sadiefreeman@gmail.com>
2018-03-19 20:57:26 +0000

    added the end section as was previously console logging

--- printing-exercise/printing-with-callbacks.js
+++ printing-exercise/printing-with-callbacks.js
@@ -5,19 +5,38 @@ var requestOptions = {
 };
 
 function getHTML (options, callback) {
+  //LINEAR VERSION
+  // const response = https.get(options);
+  // response.setEncoding('utf8');
+  // var newData ='';
+  // while(!response.on('end')){
+  //   var data = response.on('data');
+  //   newData += data;
+  // }
+  // return newData;
+
  https.get(options, function (response) {
     var newData = "";
     response.setEncoding('utf8');
 
+    // We are constructing HTML that will be consumed by callback
     response.on('data', function (data) {
-      console.log(data += newData);
+      newData += data;
     });
- })
+
+    response.on('end', function(){
+      // Hey, callback, whatever you were going to do with this,
+      // you can do it now.
+
+      // Please don't tell me.
+      callback(newData);
+    });
+ });
 }
 
 
 function printHTML (html) {
-  console.log(html);
+  console.log('Print HTML', html);
 }
 
 getHTML(requestOptions, printHTML);
